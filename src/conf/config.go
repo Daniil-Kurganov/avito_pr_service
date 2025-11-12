a26cad1ebddab7853824f2b8152a131f2af9a4b5
Daniil Kurganov <dk30128@gmail.com>
2025-11-12 23:32:23 +0300

    Implemented logger initialization

--- src/conf/config.go
+++ src/conf/config.go
@@ -1,7 +1,14 @@
 package conf
 
-import "log/slog"
+import (
+	"log/slog"
+	"os"
+)
 
 const ServerHTTPServeSocket = "127.0.0.1:8080"
 
 var Logger *slog.Logger
+
+func init() {
+	Logger = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug.Level()}))
+}
